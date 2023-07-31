d57ae5d7fbb8e0724213f20a24ec157427202ece
kolynzb <collinsbenda360@gmail.com>
2023-07-31 18:14:27 +0300

    feat: add create user trigger

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
@@ -17,3 +17,37 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
+// Firebase Admin SDK to access Firestore.
+admin.initializeApp();
+const db = admin.firestore();
+
+/**
+ * Create user in Firestore on user account creation through Firebase Auth.
+ */
+export const createUser = functions.auth.user().onCreate(async (user: any) => {
+  /**
+   * Cherry pick user data to only send
+   * what we actually need to the client.
+   */
+  const newUser = {
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName,
+    providerData: user.providerData,
+  };
+
+  db.collection("users").doc(user.uid).set(newUser);
+});
+
+/**
+ * Delete user in Firestore on user account deletion through Firebase Auth.
+ */
+export const deleteUserDocument = functions.auth
+  .user()
+  .onDelete(async (user: any) => {
+    db.collection("users").doc(user.uid).delete();
+  });
