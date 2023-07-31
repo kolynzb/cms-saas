68c755e149e85d0821bfd38e7d53e084f026967b
kolynzb <collinsbenda360@gmail.com>
2023-07-31 13:08:25 +0300

    feat: create miniloadig spiner component

---
+++ web/components/icons/MiniLoadingSpinner.tsx
@@ -0,0 +1,7 @@
+const MiniLoadingSpinner = () => (
+  <div className=" flex justify-center items-center">
+    <div className="animate-spin rounded-full h-7 w-7 border-b-2 border-gray-50"></div>
+  </div>
+);
+
+export default MiniLoadingSpinner;
