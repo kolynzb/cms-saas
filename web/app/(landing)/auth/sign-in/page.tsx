34b110a823f0adc84dd685476eeb5b480c85025a
kolynzb <collinsbenda360@gmail.com>
2023-07-31 13:09:42 +0300

    feat: create signi n page

--- web/app/(landing)/auth/sign-in/page.tsx
+++ web/app/(landing)/auth/sign-in/page.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+"use client";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
+import { auth } from "@config/firebase";
+import { EmailIcon, PasswordIcon } from "@components/icons";
+import site from "@config/site.json";
+import MiniLoadingSpinner from "@components/icons/MiniLoadingSpinner";
+import FieldErrorMessage from "@components/landing/forms/FieldErrorMessage";
+import SignInWithGoogleBtn from "@components/buttons/auth/SignInWithGoogleBtn";
 
 type Props = {};
 
 const SignIn = (props: Props) => {
+  const [signInForm, setSignInForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [signInWithEmailAndPassword, user, loading, fbError] =
+    useSignInWithEmailAndPassword(auth);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(signInForm.email, signInForm.password);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSignInForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+
+
   return (
     <div className="relative py-16 ">
       <div className="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40">
@@ -11,7 +40,7 @@ const SignIn = (props: Props) => {
             <div className="p-6 sm:p-16">
               <div className="space-y-4">
                 <img
-                  src="https://tailus.io/sources/blocks/social/preview/images/icon.svg"
+                  src={site.logo}
                   loading="lazy"
                   className="w-10"
                   alt="logo"
@@ -20,22 +49,8 @@ const SignIn = (props: Props) => {
                   Sign in to unlock the <br /> best of logo.
                 </h2>
               </div>
-              <div className="mt-16 grid space-y-4">
-                <button
-                  className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
-   hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
-                >
-                  <div className="relative flex items-center space-x-4 justify-center">
-                    <img
-                      src="https://tailus.io/sources/blocks/social/preview/images/google.svg"
-                      className="absolute left-0 w-5"
-                      alt="google logo"
-                    />
-                    <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-                      Continue with Google
-                    </span>
-                  </div>
-                </button>
+              <form onSubmit={handleSubmit} className="mt-16 grid space-y-4">
+             < SignInWithGoogleBtn />
                 <button
                   className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
    hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
@@ -69,7 +84,42 @@ const SignIn = (props: Props) => {
                     </span>
                   </div>
                 </button>
-              </div>
+
+                <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                  <EmailIcon />
+                  <input
+                    className="pl-2 outline-none border-none"
+                    type="email"
+                    name=""
+                    id=""
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                  <PasswordIcon />
+                  <input
+                    className="pl-2 outline-none border-none"
+                    name="password"
+                    type="password"
+                    id=""
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                </div>
+                {fbError && (
+                  <FieldErrorMessage
+                    shortMessage="field error"
+                    message={fbError.message}
+                  />
+                )}
+                <button
+                  type="submit"
+                  className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+                >
+                  {loading ? <MiniLoadingSpinner /> : "Sign In"}
+                </button>
+              </form>
 
               <div className="mt-32 space-y-4 text-gray-600 text-center sm:-mb-8">
                 <p className="text-xs">
