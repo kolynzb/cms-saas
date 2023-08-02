93451b60b4f2530c6dc0bcd47fb1e66e94c445b9
kolynzb <collinsbenda360@gmail.com>
2023-08-02 12:37:56 +0300

    feat: add onboarding screen once

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mobile/screens/home_screen.dart';
 import 'package:mobile/screens/on_boarding_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+int? isViewed;
+void main() async {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  isViewed = prefs.getInt('onBoard');
+  runApp(const CRMApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CRMApp extends StatefulWidget {
+  const CRMApp({super.key});
 
+  @override
+  _CRMAppState createState() => _CRMAppState();
+}
+
+class _CRMAppState extends State<CRMApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'CMS Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const OnBoardingScreen());
+      // debugShowCheckedModeBanner: false,
+      title: 'CMS Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: isViewed != 0 ? const OnBoardingScreen() : const HomeScreen(),
+    );
   }
 }
