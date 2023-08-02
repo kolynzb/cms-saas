fcc0ca992488b5315d9956d8f9d62be362720bba
kolynzb <collinsbenda360@gmail.com>
2023-08-02 12:38:40 +0300

    feat: and onboarding screens

--- mobile/lib/screens/on_boarding_screen.dart
+++ mobile/lib/screens/on_boarding_screen.dart
@@ -11,26 +11,26 @@ class OnBoardingScreen extends StatelessWidget {
         OnboardingPageModel(
           title: 'Fast, Fluid and Secure',
           description: 'Enjoy the best of the world in the palm of your hands.',
-          image: 'assets/image0.png',
+          image: 'assets/onboarding/image0.png',
           bgColor: Colors.indigo,
         ),
         OnboardingPageModel(
           title: 'Connect with your friends.',
           description: 'Connect with your friends anytime anywhere.',
-          image: 'assets/image1.png',
+          image: 'assets/onboarding/image1.png',
           bgColor: const Color.fromARGB(255, 0, 105, 82),
         ),
         OnboardingPageModel(
           title: 'Bookmark your favorite',
           description:
               'Bookmark your favorite quotes to read at a leisure time.',
-          image: 'assets/image2.png',
+          image: 'assets/onboarding/image2.png',
           bgColor: const Color(0xfffeae4f),
         ),
         OnboardingPageModel(
           title: 'Follow creators',
           description: 'Follow your favorite creators to stay in the loop.',
-          image: 'assets/image3.png',
+          image: 'assets/onboarding/image3.png',
           bgColor: Colors.purple,
         ),
       ],
