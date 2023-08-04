091d6e706d7130560edb498d424f3eb88dc337bf
kolynzb <collinsbenda360@gmail.com>
2023-08-04 13:31:46 +0300

    feat: create lead type

--- web/interfaces/kanban.interface.ts
+++ web/interfaces/kanban.interface.ts
@@ -1,4 +1,23 @@
 export interface IPipelineCard {
   id: string;
-  priority:string;
+  priority: string;
+}
+
+export interface Assignee {
+  avatar: string;
+}
+
+export interface ILead {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  priority: string;
+  status: string;
+  company: string;
+  created: string;
+  lastContacted: string;
+  expectedClose: string;
+  estimatedValue: string;
+  assignees: Assignee[];
 }
