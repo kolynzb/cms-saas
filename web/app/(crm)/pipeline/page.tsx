b7d9f64df85172c0a58dac91d0349babe9231b20
kolynzb <collinsbenda360@gmail.com>
2023-08-04 06:24:36 +0300

    feat: create leads pipeline table

--- web/app/(crm)/pipeline/page.tsx
+++ web/app/(crm)/pipeline/page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import LeadData from "@data/pipeline/leads.json";
+import StatusData from "@data/pipeline/status.json";
+import Board from "@components/Board";
 
 type Props = {};
-
 const Page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="lg:px-10 mt-6">
+        <h1 className="text-2xl font-bold dark:text-gray-700">
+          Your Leads Pipeline
+        </h1>
+      </div>
+      {/* Add Tabs */}
+      <Board
+        title={"Your Leads Board"}
+        columnData={StatusData}
+        leadData={LeadData}
+      />
+    </>
+  );
 };
 
 export default Page;
