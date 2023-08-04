e72aac69a16dacbc55cf89c4795d3f42eb9a737a
kolynzb <collinsbenda360@gmail.com>
2023-08-04 14:26:00 +0300

    feat: create providers for crm

---
+++ web/components/CRMProviders.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+import { TooltipProvider } from "./ToolTip/provider";
+
+export function CRMProviders({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>
+      <TooltipProvider>{children}</TooltipProvider>
+    </NextThemesProvider>
+  );
+}
