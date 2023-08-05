b8101c2946e49209a2a65e4e96b298f146f7316f
kolynzb <collinsbenda360@gmail.com>
2023-08-05 22:48:48 +0300

    feat: add lib to domains

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com","images.unsplash.com","randomuser.me"],
+    domains: ["res.cloudinary.com","images.unsplash.com","randomuser.me","i.ibb.co"],
   },
 };
 
