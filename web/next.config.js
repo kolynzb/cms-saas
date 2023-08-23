3b91f37a6d1ebaa0d4d142353b7176abf44499a1
kolynzb <collinsbenda360@gmail.com>
2023-08-23 20:12:15 +0300

    chore: add google domain

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com","images.unsplash.com","randomuser.me","i.ibb.co"],
+    domains: ["res.cloudinary.com","images.unsplash.com","randomuser.me","i.ibb.co","lh3.googleusercontent.com"],
   },
 };
 
