d30aed9e9a754f7c6a9f1ca77d2a6da644db4140
kolynzb <collinsbenda360@gmail.com>
2023-08-25 10:54:43 +0300

    feat: update multi step form

--- web/hooks/use-multi-step-form-hook.tsx
+++ web/hooks/use-multi-step-form-hook.tsx
@@ -1,33 +1,34 @@
-import { ReactElement, useState } from "react";
+import { ReactElement, useState } from "react"
 
+export function useMultiStepForm(steps: ReactElement[]) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
 
-export function useMultiStepForm(steps:ReactElement[]){
- const [currentStepIndex, setCurrentStepIndex] = useState<number>(0)
-
- function next (){
-setCurrentStepIndex(i =>{
-    if(i >= steps.length -1) return i
-    return i + 1
-})
- }
-
- function back(){
-    setCurrentStepIndex(i =>{
-        if(i <= 0) return i
-        return i - 1
+  function next() {
+    setCurrentStepIndex(i => {
+      if (i >= steps.length - 1) return i
+      return i + 1
     })
- }
+  }
 
- function goTo(index:number){
+  function back() {
+    setCurrentStepIndex(i => {
+      if (i <= 0) return i
+      return i - 1
+    })
+  }
+
+  function goTo(index: number) {
     setCurrentStepIndex(index)
- }
+  }
 
- return {
+  return {
     currentStepIndex,
-    step:steps[currentStepIndex],
-    goTo,next,back
- }
+    step: steps[currentStepIndex],
+    steps,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    goTo,
+    next,
+    back,
+  }
 }
-
-// https://youtu.be/uDCBSnWkuH0
-// https://youtu.be/F2hvAqyrqMI
