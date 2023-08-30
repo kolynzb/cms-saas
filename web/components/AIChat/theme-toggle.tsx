1c47a4cea17f1492a1d725efbcf78b180795a998
kolynzb <collinsbenda360@gmail.com>
2023-08-31 00:30:42 +0300

    feat: add them toggle button

--- web/components/AIChat/theme-toggle.tsx
+++ web/components/AIChat/theme-toggle.tsx
@@ -1,14 +1,13 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { useTheme } from 'next-themes'
-
-import { Button } from './button'
-import { IconMoon, IconSun } from './icons'
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { RiMoonFill, RiMoonLine, RiSunFill, RiSunLine } from "react-icons/ri";
+import { Button } from "./button";
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme()
-  const [_, startTransition] = React.useTransition()
+  const { setTheme, theme } = useTheme();
+  const [_, startTransition] = React.useTransition();
 
   return (
     <Button
@@ -16,16 +15,16 @@ export function ThemeToggle() {
       size="icon"
       onClick={() => {
         startTransition(() => {
-          setTheme(theme === 'light' ? 'dark' : 'light')
-        })
+          setTheme(theme === "light" ? "dark" : "light");
+        });
       }}
     >
-      {!theme ? null : theme === 'dark' ? (
-        <IconMoon className="transition-all" />
+      {!theme ? null : theme === "dark" ? (
+        <RiMoonLine className="transition-all h-4 w-4 text-gray-600 dark:text-white" />
       ) : (
-        <IconSun className="transition-all" />
+        <RiSunLine className="transition-all h-4 w-4 text-gray-600 dark:text-white" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
-  )
+  );
 }
