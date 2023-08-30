57d9b8d771b5f09db3e7ea25461f3af946e57071
kolynzb <collinsbenda360@gmail.com>
2023-08-31 00:16:49 +0300

    chore: add dark mode

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "media", // or 'class'
+  darkMode: "class",
   theme: {
     extend: {
       width: {
