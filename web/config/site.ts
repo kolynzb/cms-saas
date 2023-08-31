5dc9b2ddb0aa4a8a255c4378a21a88ca311b3e1e
kolynzb <collinsbenda360@gmail.com>
2023-08-31 13:50:30 +0300

    chore: add dummy logos

--- web/config/site.ts
+++ web/config/site.ts
@@ -1,17 +1,17 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Founders Lounge",
-	description: "Make beautiful websites regardless of your design experience.",
-	logo:{
-        dark:"/logo.png",
-        light:"/logo.png"
-    },
-	url:"https://precedent.dev",
-	navItems: [
+  name: "Founders Lounge",
+  description: "Make beautiful websites regardless of your design experience.",
+  logo: {
+    dark: "https://tailus.io/sources/blocks/social/preview/images/icon.svg",
+    light: "https://tailus.io/sources/blocks/social/preview/images/icon.svg",
+  },
+  url: "https://precedent.dev",
+  navItems: [
     {
-			label: "Home",
-			href: "/",
+      label: "Home",
+      href: "/",
     },
     {
       label: "Docs",
@@ -28,47 +28,47 @@ export const siteConfig = {
     {
       label: "About",
       href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-        sponsor: "https://patreon.com/jrgarciadev"
-	},
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
 };
