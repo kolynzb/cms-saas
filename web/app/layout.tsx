26258c2d43fe153b45bd82c40f1a26680eec1327
kolynzb <collinsbenda360@gmail.com>
2023-08-31 13:48:52 +0300

    feat: add site metadata and  global providers

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,48 @@
 "use client";
 import "@styles/globals.css";
 import React from "react";
+import { Metadata } from "next";
+import { siteConfig } from "@config/site";
+import { Providers } from "./providers";
+import { TailwindIndicator } from "@components/TailwindIndicator";
+
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  authors: [{ name: "Atuhaire Collins Benda", url: "benda.vercel.app" }],
+  twitter: {
+    card: "summary_large_image",
+    title: `${siteConfig.name} - ${siteConfig.description}`,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/opengraph-image`],
+    creator: "@Kolynz_b",
+  },
+  openGraph: {
+    title: `${siteConfig.name} - ${siteConfig.description}`,
+    description: siteConfig.description,
+    images: [
+      {
+        url: `${siteConfig.url}/opengraph-image`,
+      },
+    ],
+  },
+  metadataBase: new URL(siteConfig.url),
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
+
+};
 
 export default function RootLayout({
   children,
@@ -8,8 +50,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <html lang="en">
+    <body>
+      <Providers attribute="class" defaultTheme="system" enableSystem>
       {children}
-    </>
+      <TailwindIndicator />
+      </Providers>
+    </body>
+    </html>
   );
 }
